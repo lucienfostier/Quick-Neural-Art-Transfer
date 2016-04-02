27630d87bdc4763cea192bd4f04b10f669698824
Tzer-jen Wei <tzerjen@gmail.com>
2016-04-02 09:44:00 +0800

    add  flip_filters=False to cpu convlayer

--- neuralStyle.py
+++ neuralStyle.py
@@ -30,7 +30,10 @@ try:
     from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
 except:
     print("unable to import Conv2DDNNLayer, use Conv2DLayer instead")
-    from lasagne.layers import Conv2DLayer as ConvLayer
+    from lasagne.layers import Conv2DLayer as ConvLayer0
+    def ConvLayer(*args, **kwargs):
+        return ConvLayer0(*args, , flip_filters=False, **kweargs)
+        
 from lasagne.layers import Pool2DLayer as PoolLayer
 from lasagne.nonlinearities import softmax
 
